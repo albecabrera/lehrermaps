3b18db602d99a3d403718bc3d408644b0f3ffbca
albecabrera <albe.cabrera@yahoo.de>
2026-05-21 08:34:05 +0200

    fix: reset viewMode to subjects when switching subject tabs from Stundenplan

--- lehrermaps/client/src/pages/App.jsx
+++ lehrermaps/client/src/pages/App.jsx
@@ -140,6 +140,7 @@ export default function App({ onLogout }) {
     setActiveFolder(null);
     setActiveFile(null);
     setQuery('');
+    setViewMode('subjects');
   };
 
   const onFolderSelect = (folder) => {
