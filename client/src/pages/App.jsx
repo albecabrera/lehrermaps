24a71f72d3fc95b2bcdb9d4c8b24aa70e3c6c608
albecabrera <albe.cabrera@yahoo.de>
2026-09-23 07:49:45 +0200

    feat(sidebar): add click and teach launchers

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -55,6 +55,8 @@ const PLESK_TERMINAL_URL = 'https://h2953700.stratoserver.net:8443/modules/ssh-t
 
 const EXTERNAL_APP_RAIL_LAUNCHERS = [
   { id: 'click-and-teach-5-6', name: 'click & teach 5/6', href: 'https://www.click-and-teach.de/Player/id/1280/page/8', label: 'click & teach 5/6 öffnen', iconSrc: 'https://www.click-and-teach.de/img/CCBLogo.png', iconClass: 'wide' },
+  { id: 'click-and-teach-7-8', name: 'click & teach 7/8', href: 'https://www.click-and-teach.de/Player/id/1259/page/121', label: 'click & teach 7/8 öffnen', iconSrc: 'https://www.click-and-teach.de/img/CCBLogo.png', iconClass: 'wide' },
+  { id: 'click-and-teach-9-10', name: 'click & teach 9/10', href: 'https://www.click-and-teach.de/Player/id/1399/page/61', label: 'click & teach 9/10 öffnen', iconSrc: 'https://www.click-and-teach.de/img/CCBLogo.png', iconClass: 'wide' },
   { id: 'ucs', name: 'UCS', href: 'https://master.schulen-bn.de/univention/management/#module=schoolusers:student:0:', label: 'UCS öffnen', iconSrc: '/assets/ucs-logo.png', iconClass: 'wide' },
   { id: 'anton', name: 'ANTON', href: 'https://anton.app/', label: 'ANTON öffnen', iconSrc: '/assets/anton-favicon.ico' },
   { id: 'vamos-1', name: 'Vamos adelante 1', href: 'https://bridge.klett.de/DUA-W9ISFVJLTT/?page=1', label: 'Vamos adelante 1 öffnen', iconSrc: '/assets/klett-favicon.ico' },
